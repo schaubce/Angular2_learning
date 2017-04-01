60fe31bd5c969d40a482d1876536e027b3b2baf7
Schaub <n0268446@lm.lmig.com>
2017-04-01 19:20:52 -0400

    Creating a service to fill in our products

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component } from '@angular/core';
+import {ProductService} from "./products/product.service";
 
 @Component({
     selector: 'pm-app',
@@ -6,7 +7,11 @@ import {Component } from '@angular/core';
     `<div>
         <h1>{{pageTitle}}</h1>
         <pm-products></pm-products>
-    </div>`
+    </div>`,
+
+    //adding the service to the root component so it can be used by all components
+    // this can be injected within a constructor within a class
+    providers: [ProductService]
 })
 
 export class AppComponent{
